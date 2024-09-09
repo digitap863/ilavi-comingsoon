2cfd30dc815e0880454a61091a7824134ebefdd0
Ajmal-Ali-k <ajmalali749@gmail.com>
2024-09-09 14:35:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import comingsoon from "./assets/comingsoon.jpg"
+
+function App() {
+
+
+  return (
+    <div className='h-screen w-full bg-gradient-to-t from-[#D3C4B8] to-[#D3C8BD] '>
+      <img src={comingsoon} alt="" className='w-full h-full object-co' />
+    </div>
+  )
+}
+
+export default App
